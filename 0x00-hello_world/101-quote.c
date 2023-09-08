8e5c504b5b089fae055c3609d73b92a9135953db
Ki11ay <mohammedaliedriis@gmail.com>
2023-09-07 19:15:42 -0700

    done

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -4,6 +4,8 @@
  *main - entry point
  *
  *Description : a C program which print a quote using write function
+ * write(int fd,const void buf,size_t count)
+ *
  *Return: 1 (Not Successful)
 */
 int main(void)
