35a94af462c7b7009e4833139ab9e33a55c39448
Ki11ay <mohammedaliedriis@gmail.com>
2023-09-08 09:20:52 -0700

    a

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+
+/**
+ *main - entry point
+ *Description: a program that prints the numbers using while loop and 4 putchar
+ *Return: 0 (Success)
+*/
+int main(void)
+{
+	int i = 0;
+	int j = 0;
+
+	while (i <= 9)
+	{
+		while (j <= 9)
+		{
+			if (i != j && i < j)
+			{
+				putchar(i + '0');
+				putchar(j + '0');
+				if (i + j != 17)
+				{
+					putchar(',');
+					putchar(' ');
+				}
+			}
+			j++;
+		}
+		i++
+	}
+	putchar('\n');
+	return (0);
+}
