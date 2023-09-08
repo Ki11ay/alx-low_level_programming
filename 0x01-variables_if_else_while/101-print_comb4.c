ac062d68d1a3925cb746bab310ead37bbca1c9a8
Ki11ay <mohammedaliedriis@gmail.com>
2023-09-08 09:48:37 -0700

    a

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -30,7 +30,7 @@ int main(void)
                                         putchar(' ');
                                 }
                         }
-			k++
+			k++;
 			}
                         j++;
                 }
