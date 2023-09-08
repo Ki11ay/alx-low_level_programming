a1bd445bd40dbccbfe4bb472062435f412d505f6
Ki11ay <mohammedaliedriis@gmail.com>
2023-09-08 08:57:53 -0700

    a

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -9,13 +9,16 @@ int main(void)
 {
 	int i = 0;
 
-	while (i <= 8)
+	while (i <= 9)
 	{
 		putchar(i + '0');
+		if (i != 9)
+		{
 		putchar(',');
 		putchar(' ');
+		}
 		i++;
 	}
-	putchar('9');
+	putchar('\n');
 	return (0);
 }
