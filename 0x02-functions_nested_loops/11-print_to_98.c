9effff70b2003e59ceb14755b1b67c7524ec457c
Ki11ay <mohammedaliedriis@gmail.com>
2023-09-12 12:10:42 -0700

    ‘a’

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -10,10 +10,10 @@ void print_to_98(int i)
 	int s;
 
 	if (i > 98)
-		for (s = 0; s > 98; s--)
+		for (s = i; s > 98; s--)
 			printf("%d, ", s);
 	else if (i < 98)
-		for (s = 0; s < 98; s++)
+		for (s = i; s < 98; s++)
 			printf("%d, ", s);
 	printf("98\n");
 }
