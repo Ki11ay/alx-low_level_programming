bdab08ee36682984b88f7fee4738ff49bd861062
Ki11ay <mohammedaliedriis@gmail.com>
2023-09-19 09:04:01 -0700

    p

---
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+/**
+ *main - generates random passwords for 101-crackme
+ *Return: 0 
+ *
+*/
+
+int main(void)
+{
+	int s;
+	char c;
+
+	srand(time(NULL));
+	while (s <= 2645)
+	{
+		c = rand() % 128;
+		s += c;
+		putchar(c);
+	}
+	putchar(2348 - s);
+	return (0);
+}
