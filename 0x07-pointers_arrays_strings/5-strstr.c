e582af52a320ce10093bd6c711201be8b2680e08
Ki11ay <mohammedaliedriis@gmail.com>
2023-09-25 12:54:49 -0700

    7

---
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * *_strstr - a function that locates a substring.
+ *@haystack: the string we want to search in
+ *@needle: the substring we want to search for
+ *Return: a string
+*/
+
+char *_strstr(char *haystack, char *needle)
+{
+
+	for ()
+	return (s);
+}
---
+++ 0x07-pointers_arrays_strings/8-print_diagsums.c
@@ -0,0 +1,23 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ *print_diagrams - prints the sum of the two diagonals of a square matrix
+ *@a: Two dimension integer array
+ *@size: the size of square array
+ *Return: nothing
+*/
+
+void print_diagrams(int *a, int size)
+{
+	int i, d1 = 0, d2 = 0;
+
+	for (i = 0; i < size; i++)
+	{
+		d1 += a[i];
+		d2 += a[size - i - 1];
+		a += size;
+	}
+	printf("%d,", d1);
+	printf("%d\n", d2);
+}
