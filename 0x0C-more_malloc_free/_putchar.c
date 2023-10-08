f2db123ba332d444b8a6c34ee5c6615963c91d8a
Ki11ay <mohammedaliedriis@gmail.com>
2023-10-08 15:52:58 -0700

    last chance

--- 0x0C-more_malloc_free/_putchar.c
+++ 0x0C-more_malloc_free/_putchar.c
@@ -1,15 +1,13 @@
 #include <unistd.h>
-#include "main.h"
 
 /**
- * _putchar - writes the character m to stdout
- * @m: The character to print
- * Return: On success 1.
+ *_putchar - writes the character m to stdout
+ *@m: The character to print
+ *Return: On success 1.
  *         On error, -1 is returned, and error is set approprilately
- *
 */
 
-int _putchar(char m)
+int _putchar(char c)
 {
-	return (write(1, &m, 1));
+	return (write(1, &c, 1));
 }
